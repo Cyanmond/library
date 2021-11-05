f78b6423ffd144bb61756eff486018de53955a6c
Cyanmond <isane.sekiguchi@gmail.com>
2021-11-05 13:40:16 +0900

    add: new verify file

---
+++ verify/library-checker/many_aplusb.test.cpp
@@ -0,0 +1,19 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/many_aplusb"
+
+#include <iostream>
+#include <vector>
+
+#include "../utility/eoln.cpp"
+#include "../utility/int_alias.cpp"
+#include "../utility/rep.cpp"
+#include "../utility/scan.cpp"
+
+int main(void) {
+    const usize T = scan<usize>();
+    std::vector<i64> A(T), B(T);
+    for (const auto i : rep(0, T)) {
+        A[i] = scan<i64>();
+        B[i] = scan<i64>();
+        std::cout << A[i] + B[i] << eoln;
+    }
+}
