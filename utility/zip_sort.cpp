4e5c377f289f98ce6d2ae0f62ca7a53256756ce6
Cyanmond <isane.sekiguchi@gmail.com>
2021-11-07 20:31:16 +0900

    fix zip_sort.cpp

--- utility/zip_sort.cpp
+++ utility/zip_sort.cpp
@@ -4,6 +4,18 @@
 #include <algorithm>
 #include <vector>
 
+namespace helper {
+template <class T, class... Tail>
+void zip_sort_renumber(const std::vector<usize> &order, std::vector<T> &head, Tail &...tail) {
+    const usize n = order.size();
+    std::vector<T> sorted_head(n);
+    for (usize i = 0; i < n; ++i)
+        sorted_head[i] = head[order[i]];
+    head = std::move(sorted_head);
+    if constexpr (sizeof...(Tail) != 0) zip_sort_renumber(order, tail...);
+}
+} // namespace helper
+
 template <class Head, class... Tail>
 std::vector<usize> zip_sort(std::vector<Head> &head, std::vector<Tail> &...tail) {
     const usize n = head.size();
