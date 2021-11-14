9b67829363f0ac331745675836a81629f018f4ef
Cyanmond <isane.sekiguchi@gmail.com>
2021-11-14 15:49:35 +0900

    add pow_mod.cpp

---
+++ math/pow_mod.cpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "../utility/int_alias.cpp"
+#include "barrett.cpp"
+#include "nomarlize_mod.cpp"
+#include <cassert>
+#include <type_traits>
+
+template <class T> u32 pow_mod(T x, u64 exp, const u32 mod) {
+    assert(mod != 0);
+    if (mod == 1) return 0;
+    const Barrett bt(mod);
+    u32 ret = 1, p = normalize_mod<std::common_type<T, i64>>(x, mod);
+    while (exp != 0) {
+        if (exp & 1) ret = bt.product(ret, p);
+        exp >>= 1;
+        p = bt.product(ret, p);
+    }
+    return ret;
+}
