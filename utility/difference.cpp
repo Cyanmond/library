edfaa9f8e8141d3a7b214bb90c2189efc610f574
Cyanmond <isane.sekiguchi@gmail.com>
2021-11-17 22:17:39 +0900

    format

--- utility/difference.cpp
+++ utility/difference.cpp
@@ -1,5 +1,5 @@
 #pragma once
 
-template <typename T> constexpr T difference(const T a,const T b){
+template <typename T> constexpr T difference(const T a, const T b) {
     return (a > b) ? (a - b) : (b - a);
 }
