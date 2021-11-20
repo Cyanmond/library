089ca42e20d814b6368f3f2becf48121fe04ed47
Cyanmond <isane.sekiguchi@gmail.com>
2021-11-21 02:00:13 +0900

    modify

--- utility/revrep.cpp
+++ utility/revrep.cpp
@@ -1,21 +1,32 @@
 #pragma once
 
 #include "../utility/int_alias.cpp"
+#include <algorithm>
 
 class revrep {
     struct revrep_iterator {
         usize itr;
         constexpr revrep_iterator(const usize pos) noexcept : itr(pos) {}
-        constexpr void operator++() noexcept { --itr; }
-        constexpr bool operator!=(const usize &other) const noexcept { return itr != other; }
-        constexpr usize operator*() const noexcept { return itr; }
+        constexpr void operator++() noexcept {
+            --itr;
+        }
+        constexpr bool operator!=(const usize &other) const noexcept {
+            return itr != other;
+        }
+        constexpr usize operator*() const noexcept {
+            return itr;
+        }
     };
     const revrep_iterator first;
     const usize last;
 
   public:
-    constexpr revrep(const usize first_, const usize last_) noexcept
-        : first(last_ - 1), last(first_ - 1) {}
-    constexpr revrep_iterator begin() const noexcept { return first; }
-    constexpr usize end() const noexcept { return last; }
+    constexpr revrep(const usize last_, const usize first_) noexcept
+        : first(first_ - 1), last(std::min(first_, last_) - 1) {}
+    constexpr revrep_iterator begin() const noexcept {
+        return first;
+    }
+    constexpr usize end() const noexcept {
+        return last;
+    }
 };
--- utility/scan_vector.cpp
+++ utility/scan_vector.cpp
@@ -1,21 +1,34 @@
 #pragma once
 
 #include "int_alias.cpp"
+#include "scan.cpp"
 #include <iostream>
 #include <vector>
 
-template <typename T> inline std::vector<T> scan_vec(const usize n) {
+template <typename T> std::vector<T> scan_vec(const usize n) {
     std::vector<T> res(n);
-    for (usize i = 0; i < n; ++i) {
-        res[i] = scan<T>();
-    }
+    for (auto &e : res)
+        e = scan<T>();
     return res;
 }
 
-template <typename T, class F> inline std::vector<T> scan_vec(const usize n, const F f) {
-    std::vector<T> res(n);
-    for (usize i = 0; i < n; ++i) {
-        res[i] = f(scan<T>());
-    }
+template <typename T, class... Args> auto scan_vec(const usize n, Args... args) {
+    std::vector<decltype(scan_vec(args...))> res(n);
+    for (auto &vec : res)
+        vec = scan_vec(args...);
+    return res;
+}
+
+template <typename T, class F> std::vector<T> scan_vec_f(const usize n, const F &f) {
+    std::vector<T> res;
+    for (auto &e : res)
+        e = f(scan<T>());
+    return res;
+}
+
+template <typename T, class... Args> auto scan_vec_f(const usize n, Args... args) {
+    std::vector<decltype(scan_vec(args...))> res(n);
+    for (auto &vec : res)
+        vec = scan_vec(args...);
     return res;
 }
